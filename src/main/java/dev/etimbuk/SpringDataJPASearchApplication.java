46e111e7db0657b5aba173d423829ee56de59dfb
gogetter <Gogetter@users.noreply.github.com>
2025-09-14 21:02:35 +0100

    initial commit

---
+++ src/main/java/dev/etimbuk/SpringDataJPASearchApplication.java
@@ -0,0 +1,13 @@
+package dev.etimbuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJPASearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJPASearchApplication.class, args);
+	}
+
+}
